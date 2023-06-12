72ec94e9cd4aeb7bdfeae2796adc8804e1c69e7a
Nikita Kryukov <cravtos92@gmail.com>
2023-06-12 13:41:21 +0700

    add more error handling

--- server.c
+++ server.c
@@ -1,46 +1,87 @@
-// TODO: do not forget about error handling (https://beej.us/guide/bgnet/html/split/man-pages.html#errnoman)
+// used as reference: https://beej.us/guide/bgnet/html/split/system-calls-or-bust.html
+
+// TODO: handle select() erronious ready-to-read with O_NONBLOCK
 
 #include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
 #include <string.h>
 #include <stddef.h>
 #include <netdb.h>
 #include <stdio.h>
 #include <errno.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 #define PORT "31337"
 #define MAX_PENDING 50
 
+void *get_in_addr(struct sockaddr *sa) {
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
 
 int main() {
-    struct addrinfo hints, *res;
-    int sockfd;
-
+    struct addrinfo hints;
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
 
-    getaddrinfo(NULL, PORT, &hints, &res);
-    sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-    if (sockfd == -1) {
-        perror("socket");
-        return -1;
+    int rv = 0;
+    struct addrinfo *ai, *p;
+    getaddrinfo(NULL, PORT, &hints, &ai);
+    if ((rv = getaddrinfo(NULL, PORT, &hints, &ai)) != 0) {
+        fprintf(stderr, "selectserver: %s\n", gai_strerror(rv));
+        exit(1);
     }
 
-    bind(sockfd, res->ai_addr, res->ai_addrlen);
+    int sockfd = 0;
+    for (p = ai; p != NULL; p = p->ai_next) {
+        sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+        if (sockfd < 0) {
+            continue;
+        }
+
+        int yes = 1;
+        setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) < 0) {
+            close(sockfd);
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL) {
+        fprintf(stderr, "selectserver: failed to bind\n");
+        exit(2);
+    }
+
+    freeaddrinfo(ai);
 
     if (listen(sockfd, MAX_PENDING) == -1) {
         perror("listen");
-        return -1;
+        exit(1);
     }
 
     struct sockaddr_storage their_addr;
     socklen_t addr_size = sizeof their_addr;
-    char* hello_msg = "Hello!\n";
+    char buf[32];
     for (int i = 0; i < 5; i++) {
         int fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
-        send(fd, hello_msg, strlen(hello_msg), 0);
+        int n = recv(fd, buf, sizeof buf - 1, 0);
+        if (n == -1) {
+            perror("recv");
+            goto next;
+        }
+        buf[n] = '\0';
+        send(fd, buf, strlen(buf), 0);
+next:
         close(fd);
     }
 
