9dd85c96e3692ebaef3b88a88b536a831ebaf7be
Nikita Kryukov <cravtos92@gmail.com>
2023-06-12 15:44:50 +0700

    add select of sockets

--- server.c
+++ server.c
@@ -1,10 +1,10 @@
 // used as reference: https://beej.us/guide/bgnet/html/split/system-calls-or-bust.html
 
-// TODO: handle select() erronious ready-to-read with O_NONBLOCK
-
 #include <sys/socket.h>
 #include <sys/time.h>
 #include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <string.h>
 #include <stddef.h>
 #include <netdb.h>
@@ -12,6 +12,7 @@
 #include <errno.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <fcntl.h>
 
 #define PORT "31337"
 #define MAX_PENDING 50
@@ -24,14 +25,14 @@ void *get_in_addr(struct sockaddr *sa) {
     return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
-int main() {
+int start_server() {
     struct addrinfo hints;
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
 
-    int rv = 0;
+    int rv;
     struct addrinfo *ai, *p;
     getaddrinfo(NULL, PORT, &hints, &ai);
     if ((rv = getaddrinfo(NULL, PORT, &hints, &ai)) != 0) {
@@ -69,20 +70,74 @@ int main() {
         exit(1);
     }
 
-    struct sockaddr_storage their_addr;
-    socklen_t addr_size = sizeof their_addr;
-    char buf[32];
-    for (int i = 0; i < 5; i++) {
-        int fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
-        int n = recv(fd, buf, sizeof buf - 1, 0);
-        if (n == -1) {
-            perror("recv");
-            goto next;
+    return sockfd;
+}
+
+int main() {
+    int sockfd = start_server();
+    int maxfd = sockfd;
+
+    fd_set master;
+    fd_set temp_fd;
+
+    FD_ZERO(&temp_fd);
+    FD_ZERO(&master);
+    FD_SET(sockfd, &master);
+
+    for (;;) {
+        temp_fd = master;
+        if (select(maxfd+1, &temp_fd, NULL, NULL, NULL) == -1) {
+            perror("select");
+            exit(1);
         }
-        buf[n] = '\0';
-        send(fd, buf, strlen(buf), 0);
-next:
-        close(fd);
+
+        for (int fd = 0; fd <= maxfd; fd++) {
+            if (!FD_ISSET(fd, &temp_fd)) {
+                continue;
+            }
+
+            // handle new connection
+            if (fd == sockfd) {
+                struct sockaddr_storage remote_addr;
+                socklen_t addr_size = sizeof remote_addr;
+                int newfd = accept(sockfd, (struct sockaddr *)&remote_addr, &addr_size);
+                if (newfd == -1) {
+                    perror("accept");
+                    continue;
+                }
+                
+                FD_SET(newfd, &master);
+                maxfd = (newfd >= maxfd) ? newfd : maxfd;
+                char remoteIP[INET6_ADDRSTRLEN];
+                printf("selectserver: new connection from %s on "
+                            "socket %d\n",
+                            inet_ntop(remote_addr.ss_family,
+                                get_in_addr((struct sockaddr*)&remote_addr),
+                                remoteIP, INET6_ADDRSTRLEN),
+                            newfd);
+                continue;
+            }
+
+            // handle data from a client
+            char buf[256];
+            int n = recv(fd, buf, sizeof buf - 1, 0);
+            if (n <= 0) {
+                if (n == 0) {
+                    printf("selectserver: socket %d hung up\n", fd);
+                } else {
+                    perror("recv");
+                }
+                close(fd);
+                FD_CLR(fd, &master);
+                continue;
+            }
+            buf[n] = '\0';
+
+            printf("got data from socket %d: %s", fd, buf);
+            if (buf[n-1] != '\n') {
+                puts("");
+            }
+        }
     }
 
     close(sockfd);
